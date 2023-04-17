5f721a25520a92e610f00735200f9cbe6cd4a0fd
Mahabub Alam <mahabubx7@gmail.com>
2023-04-17 19:44:04 +0600

    :rocket: setup:webpack

---
+++ src/app.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import './styles.scss'; // styles -> css or scss
+import { setupApp } from './modules';
+
+setupApp(document.querySelector('#app')); // mounting our app
---
+++ src/modules/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/extensions */
+import setupApp from './setup.js';
+
+export { setupApp };
