92b718f41fba5897a3f56040c914be6b84b10215
Mahabub Alam <mahabubx7@gmail.com>
2023-04-18 10:20:35 +0600

    :rocket: added:header-component

--- src/modules/setup.js
+++ src/modules/setup.js
@@ -1,16 +1,23 @@
 /* eslint-disable import/extensions */
 
+import headerComponent from '../components/header';
+
 const renderApp = async (appContainer) => {
+  const { body } = document;
+  // header-init
+  body.insertBefore(document.createElement('header'), document.querySelector('main'));
+  headerComponent(body.querySelector('header')); // header-component-render
+
   const markups = `
     <div class="container">
-      <h1>Food Hobbies</h1>
+      <h1>Food Hobbies <small>JS capstone project</small></h1>
     </div>
   `;
   appContainer.innerHTML = `${markups}`;
 };
 
-const setupApp = (element) => {
-  renderApp(element); // render first-initial DOM
+const setupApp = async (element) => {
+  await renderApp(element); // render first-initial DOM
 };
 
 export default setupApp;
