9111c07eef15eef8c77a997f5389b3593af92218
Fickry Iman <fickry.iman@gmail.com>
2023-04-20 19:30:14 +0700

    change commentCounter as variable and export it after

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,6 +1,8 @@
-export default async (comment, header) => {
+const commentCounter = async (comment, header) => {
   if (comment.length === 0) header.innerHTML = 'Comments (0)';
   else {
     header.innerHTML = `Comments (${comment.length})`;
   }
 };
+
+export default commentCounter;
